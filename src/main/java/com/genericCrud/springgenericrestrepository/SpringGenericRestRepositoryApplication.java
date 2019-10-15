cff880eabf9e48067d253513cdef065dfaaf3c22
dushman <dushman.d@informaticsint.com>
2019-10-15 21:05:06 +0530

    initial commit

---
+++ src/main/java/com/genericCrud/springgenericrestrepository/SpringGenericRestRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.genericCrud.springgenericrestrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGenericRestRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGenericRestRepositoryApplication.class, args);
+	}
+
+}
